1591a5e3dbec87b5d7113a3c86be415cccabafee
Howard Xu <xzhoward6@gmail.com>
2025-02-26 09:22:57 -0500

    Initial commit - Added Stats App files

--- index.js
+++ index.js
@@ -1,97 +1,32 @@
-// === DO NOT EDIT THIS REGION ===
-// Read the comments to understand how the program is structured.
+function calculateStats() {
+  let input = document.getElementById("numberInput").value;
+  
+  if (!input) {
+      alert("Please enter some numbers.");
+      return;
+  }
 
-// Prompt the user for a list of integers separated by commas.
-const userInputString = prompt(
-  "Please enter some integers separated by commas.",
-  "1,2,3,4,5"
-);
+  let numbers = input.split(',').map(num => Number(num.trim())).filter(num => !isNaN(num));
 
-// Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
+  if (numbers.length === 0) {
+      alert("Invalid input. Please enter valid numbers.");
+      return;
+  }
 
-// Convert the array of strings into an array of numbers.
-const numbers = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const number = parseInt(str);
-  numbers.push(number);
-}
+  let sum = 0, min = numbers[0], max = numbers[0];
 
-// Peform some calculations on the numbers.
-console.log(numbers);
-console.log(`You have given ${getLength(numbers)} numbers.`);
-console.log(`The sum of your numbers is ${getSum(numbers)}.`);
-console.log(`The mean of your numbers is ${getMean(numbers)}.`);
-console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
-console.log(`The largest of your numbers is ${getMax(numbers)}.`);
-console.log(`The range of your numbers is ${getRange(numbers)}.`);
-console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+  for (let num of numbers) {
+      sum += num;
+      if (num < min) min = num;
+      if (num > max) max = num;
+  }
 
-// === EDIT THE CODE BELOW ===
-// Complete the functions below to make the program work!
+  let avg = sum / numbers.length;
 
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the length of the array
- */
-function getLength(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the sum of the numbers
- */
-function getSum(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the mean of the numbers
- */
-function getMean(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the smallest of the numbers
- */
-function getMin(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the largest of the numbers
- */
-function getMax(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the range of the numbers (max - min)
- */
-function getRange(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the even numbers in the array
- */
-function getEvens(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the odd numbers in the array
- */
-function getOdds(numbers) {
-  // TODO
+  document.getElementById("results").innerHTML = `
+      <p><strong>Sum:</strong> ${sum}</p>
+      <p><strong>Average:</strong> ${avg.toFixed(2)}</p>
+      <p><strong>Minimum:</strong> ${min}</p>
+      <p><strong>Maximum:</strong> ${max}</p>
+  `;
 }
